4a4018a2c75ae142cc49b404324a28f98c527613
Andrew Perry <ajperry@pansapiens.com>
2014-05-19 12:02:57 +1000

    Fix reading of input data in predict mode. Remove examples from docopt string since it was introducing bugs.

--- analysis/predict/predict_from_comments.py
+++ analysis/predict/predict_from_comments.py
@@ -9,44 +9,31 @@ Arguments:
   INPUTFILE                     Path to a ludumscrape JSON data file
 
 Options:
-  -h --help                     Show this message
-  --version                     Show the version
-  -t, --trained=<filename>      Trained predictor input or output file.
-                                [default: trained.pickle.gz]
-  --classifier=<type>           Set the classifier type. Valid types are:
-                                KNN (k-nearest neighbors),
-                                NB (Naive Bayes),
-                                SVM (Support Vector Machine)
-                                SVR (Support Vector Regression)
-                                SLP (Single Layer Averaged Perceptron)
-                                [default: KNN]
-  --svm-kernel=<type>           Set the SVM kernel used. Valid types are:
-                                LINEAR, POLYNOMIAL and RADIAL [default: LINEAR]
-  -n <n>, --nfold=<n>           N-fold cross-validation [default: 5]
-  -f <n>, --top-features=<n>    Do feature selection via TF/IDF information gain.
-                                If zero, include all features
-                                (all included words) [default: 200]
-  --pos-tags=<tags>             Part-of-speech tag prefixes to
-                                include, comma separated J,R,N,V
-                                [default: J,R]
-  --predict=<feature>           Feature to predict. rating, rating_class,
-                                rank_class, percentile_rank,
-                                percentile_rank_class. [default: rating_class]
-  --rating-category=<category>  Overall, Graphics, Theme, Humor,
-                                Audio, Innovation [default: Overall]
-
-Example:
-  ./predict_from_comments.py train --pos-tags=J,R --classifier=SVM \\
-                                   --svm-kernel=LINEAR --top-features 200 \\
-                                   --rating-category=Graphics \\
-                                   --predict=rating_class \\
-                                   ../../results/results_ld*.json
-
-  ./predict_from_comments.py predict --trained=trained.pickle.gz  \\
-                                     --pos-tags=J,R \\
-                                     --rating-category=Graphics \\
-                                     --predict=rating_class \\
-                                     ../../results/results_ld30.json
+  -h --help                       Show this message
+  --version                       Show the version
+  -t <fn>, --trained=<filename>   Trained predictor input or output file.
+                                  [default: trained.pickle.gz]
+  --classifier=<type>             Set the classifier type. Valid types are:
+                                  KNN (k-nearest neighbors),
+                                  NB (Naive Bayes),
+                                  SVM (Support Vector Machine)
+                                  SVR (Support Vector Regression)
+                                  SLP (Single Layer Averaged Perceptron)
+                                  [default: KNN]
+  --svm-kernel=<type>             Set the SVM kernel used. Valid types are:
+                                  LINEAR, POLYNOMIAL and RADIAL [default: LINEAR]
+  -n <n>, --nfold=<n>             N-fold cross-validation [default: 5]
+  --pos-tags=<tags>               Part-of-speech tag prefixes to
+                                  include, comma separated J,R,N,V
+                                  [default: J,R]
+  --top-features=<n>              Do feature selection via TF/IDF information gain.
+                                  If zero, include all features
+                                  (all included words) [default: 200]
+  -f <str>, --feature=<feature>   Feature to predict. rating, rating_class,
+                                  rank_class, percentile_rank,
+                                  percentile_rank_class. [default: rating_class]
+  --rating-category=<category>    Overall, Graphics, Theme, Humor,
+                                  Audio, Innovation [default: Overall]
 """
 from __future__ import print_function
 
@@ -71,6 +58,11 @@ except ImportError:
 
 options = docopt(__doc__, argv=None, help=True, version=sys.argv[0] + " " + __version__)
 
+print(sys.argv[0] + " version " + __version__)
+print("Running with options:")
+print(options)
+print()
+
 trained_filename = options["--trained"]
 
 #classifier_type = "NB"
@@ -89,6 +81,7 @@ if svm_kernel == "SVR":
 n_fold = int(options["--nfold"])
 
 select_top_n_features = int(options["--top-features"])
+
 use_feature_selection = (select_top_n_features > 0)
 
 # Define with Position of Speech (POS) tags to include
@@ -106,7 +99,7 @@ rating_category = options["--rating-category"]
 
 # What should we try to predict based on the comments ?
 # The "_class" options turn it into binary classification
-predict = options["--predict"]
+predict = options["--feature"]
 #predict = "rating"
 #predict = "rating_class"
 #predict = "rank_class"
@@ -119,12 +112,6 @@ rank_class_cutoff = 10  # 10 means "In top 10 ?"
 percentile_rank_class_cutoff = 10  # 10 means "In top 10th percentile ?"
 
 #nltk_tagger = NLTKTagger()
-
-print(sys.argv[0] + " version " + __version__)
-print("Running with options:")
-print(options)
-print()
-
 
 def discard_words_without_tag_of_interest(text, tag_prefixes=tag_prefixes):
     v = parsetree(text, lemmata=True)[0]
@@ -158,22 +145,24 @@ vectors = []
 for entry in data:
     url = entry['url']
     author = entry['author']
+    output_vector = None
     # those who didn't rate anything (0% Coolness) get no Overall rating
-    if rating_category not in entry['ratings']:
-        #sys.stderr.write("Skipping: " + entry['url'] + "\n")
+    if rating_category in entry['ratings']:
+        rating = int(round(float(entry['ratings'][rating_category][0])))
+        rating_class = (rating >= rating_class_cutoff)  # binary classes
+
+        rank = int(entry['ratings'][rating_category][1])
+        rank_class = (rank <= rank_class_cutoff)  # top 10 binary classification
+
+        percentile_rank = (float(rank) / float(worst_ranking)) * 100.0
+        percentile_rank_class = (percentile_rank <= percentile_rank_class_cutoff)
+        #print(rank, percentile_rank, percentile_rank_class)
+
+        # a value of one of the above is assigned as the property to predict
+        output_vector = vars()[predict]
+    elif options["train"]:
+        sys.stderr.write("Skipping (no ratings field): " + entry['url'] + "\n")
         continue
-    rating = int(round(float(entry['ratings'][rating_category][0])))
-    rating_class = (rating >= rating_class_cutoff)  # binary classes
-
-    rank = int(entry['ratings'][rating_category][1])
-    rank_class = (rank <= rank_class_cutoff)  # top 10 binary classification
-
-    percentile_rank = (float(rank) / float(worst_ranking)) * 100.0
-    percentile_rank_class = (percentile_rank <= percentile_rank_class_cutoff)
-    #print(rank, percentile_rank, percentile_rank_class)
-
-    # a value of one of the above is assigned as the property to predict
-    output_vector = vars()[predict]
 
     all_entry_comment_text_filtered = ""
     entry_comments = []
